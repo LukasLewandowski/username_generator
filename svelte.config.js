2aa99e44cb7d516ff722cd3ffbab06f082edea88
Lukas Lewandowski <llewandowski1990@gmail.com>
2025-12-27 20:46:52 +0100

    go back to node adapter instead of static to make API requests

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,13 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
+			// output directory for the Node adapter
+			out: 'build'
 		}),
 		paths: {
 			base: process.env.BASE_PATH || ''
